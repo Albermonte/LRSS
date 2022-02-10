1cd0a4e3250da36912506a66fce0aea915c2bf42
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2022-02-10 11:18:23 +0000

    [WIP] UDP recv

--- P1.1/NOC/ping_noc_serv.c
+++ P1.1/NOC/ping_noc_serv.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <signal.h>
+
+int sock;
+
+// https://www.tutorialspoint.com/c_standard_library/c_function_signal.htm
+void sig_handler(int signum)
+{
+    if (signum == SIGINT)
+    {
+        printf("Closing %d", signum);
+        close(sock);
+        exit(1);
+    }
+}
 
 // https://www.gnu.org/software/libc/manual/html_node/Local-Socket-Example.html
 void main(int argc, char *argv[])
@@ -8,18 +22,55 @@ void main(int argc, char *argv[])
     printf("## NOC SERVER ## \n\n");
     if (argc < 2)
     {
-        printf("Faltan parámetros.\n");
+        printf("Missing param PORT.\n");
+        exit(EXIT_FAILURE);
     }
 
-    int host = atoi(argv[1]);
     // https://www.gnu.org/software/libc/manual/html_node/Socket-Addresses.html
     // https://stackoverflow.com/questions/3689925/struct-sockaddr-un-vs-sockaddr
-    struct sockaddr address;
+    struct sockaddr server_address;
+    struct sockaddr client_address;
+    int port = atoi(argv[1]);
+    char data_received[1024];
 
-    // Test msg
-    char *msg = "Hello from client";
+    printf("Listening on %d\n", port);
 
-    printf("%d\n", host);
+    // Create the socket.
+    /**
+     * socket(domain, type, protocol)
+     * domain: PF_LOCAL, AF_INET (IPv4), AF_INET6 (IPv6)
+     * type: SOCK_STREAM (TCP), SOCK_DGRAM (UDP)
+     * protocol: 0 (IP), more at /etc/protocols
+     */
+    printf("Creating socket");
+    sock = socket(PF_LOCAL, SOCK_DGRAM, 0);
+    if (sock < 0)
+    {
+        printf("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Binding socket to specified port
+    printf("Binding socket");
+    if (bind(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        printf("Binding socket to port %d failed", port);
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGINT, sig_handler);  // handle ctrl+c
+    signal(SIGTSTP, sig_handler); // handle ctrl+z
+
+    // Receive msg from client
+    // https://www.ibm.com/docs/en/zos/2.4.0?topic=sockets-using-sendto-recvfrom-calls
+    printf("Waiting for msg...");
+    if (recvfrom(sock, data_received, sizeof(data_received), 0, &client_address, sizeof(client_address)) < 0)
+    {
+        printf("Error receiving data from client");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Msg received:\n %s", data_received);
 }
 
 // https://tutorialspoint.dev/language/cpp/socket-programming-cc
